29ff4882edd9de3dbc9f66b99e868a61ad6bc7d1
Tinco-Yan <46141470+Tinco-Yan@users.noreply.github.com>
2019-03-05 15:13:24 +0800

    Add files via upload

---
+++ src/Ty.Core/Buildler/TyCoreBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ty.Core.Builder
+{
+    internal class TyCoreBuilder : ITyCoreBuilder
+    {
+        private IServiceCollection _services;
+        public IServiceCollection Services => _services;
+
+        public TyCoreBuilder(IServiceCollection services)
+        {
+            this._services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+    }
+}
