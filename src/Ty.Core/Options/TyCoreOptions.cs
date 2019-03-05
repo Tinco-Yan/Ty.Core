89533a27b8e4004bd7875f45c8a3ab2d8e1f1bca
Tinco-Yan <46141470+Tinco-Yan@users.noreply.github.com>
2019-03-05 15:16:58 +0800

    Add files via upload

--- src/Ty.Core/Options/TyCoreOptions.cs
+++ src/Ty.Core/Options/TyCoreOptions.cs
@@ -1,1 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Ty.Core.Data;
+using Ty.Core.Generic;
 
+namespace Ty.Core.Options
+{
+    public class TyCoreOptions
+    {
+        public Type DbConnectionType { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public IProvider<DataProcessor> DataProcessorProvider { get; set; }
+    }
+}
---
+++ src/Ty.Core/Options/TyCoreOptionsSetup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace Ty.Core.Options
+{
+    internal class TyCoreOptionsSetup : IConfigureOptions<TyCoreOptions>
+    {
+        public void Configure(TyCoreOptions options)
+        {
+            options.DbConnectionType = null;
+            options.ConnectionString = null;
+            options.DataProcessorProvider = null;
+        }
+    }
+}
