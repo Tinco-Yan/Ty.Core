b261e16f7bbc327b643c0b23745562af08d68f7e
Tinco-Yan <46141470+Tinco-Yan@users.noreply.github.com>
2019-03-05 15:15:03 +0800

    Add files via upload

--- src/Ty.Core/DependencyInjection/TyCoreApplicationBuilderExtensions.cs
+++ src/Ty.Core/DependencyInjection/TyCoreApplicationBuilderExtensions.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace Ty.Core.DependencyInjection
+{
+    public static class TyCoreApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UserTyCore(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
---
+++ src/Ty.Core/DependencyInjection/TyCoreServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ty.Core.Builder;
+using Ty.Core.Options;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Ty.Core.Data;
+
+namespace Ty.Core.DependencyInjection
+{
+    public static class TyCoreServiceCollectionExtensions
+    {
+        public static ITyCoreBuilder AddTyCore(this IServiceCollection services)
+        {
+            var builder = new TyCoreBuilder(services);
+
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Transient<IConfigureOptions<TyCoreOptions>, TyCoreOptionsSetup>());
+            builder.Services.AddSingleton<IDataProxy, DataProxy>();
+
+            return builder;
+        }
+
+        public static ITyCoreBuilder AddTyCore(this IServiceCollection services, Action<TyCoreOptions> setupAction)
+        {
+            var builder = services.AddTyCore();
+
+            builder.Services.Configure(setupAction);
+
+            return builder;
+        }
+    }
+}
